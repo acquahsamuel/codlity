e693b28989effd326f8bc2506db9a622f91eb69f
acquahsamuel <iamsamuelacquah@gmail.com>
2022-02-28 18:19:52 +0000

    app.js

--- app.js
+++ app.js
@@ -1,46 +1,77 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const expressValidator = require("express-validator");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const config = require("./config");
+require("./passport");
 
-var app = express();
+const indexRoute = require("./routes/index");
+const authRoute = require("./routes/auth");
+const taskRoute = require("./routes/task");
+
+mongoose.connect(config.dbConnstring);
+global.User = require("./models/User");
+global.Task = require("./models/Task");
+
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressValidator());
+app.use(
+  session({
+    secret: config.sessionKey,
+    resave: false,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', index);
-app.use('/users', users);
+//initailize session - Get authenticated user
+app.use(function (req, res, next) {
+  if (req.isAuthenticated()) {
+    res.locals.user = req.user;
+  }
+  next();
+});
+
+app.use("/", indexRoute);
+app.use("/", authRoute);
+app.use("/", taskRoute);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use(function (req, res, next) {
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
